622da7e268c31af4d535c8f3c8ab4a1859e48d78
Franco Trucco <franco.trucco@mercadolibre.com>
2020-08-31 21:17:18 -0300

    Initial commit

---
+++ app/src/main/java/android/example/gobike/ui/main/MainFragment.kt
@@ -0,0 +1,36 @@
+package android.example.gobike.ui.main
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.example.gobike.R
+import androidx.databinding.DataBindingUtil
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: MainFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.main_fragment, container, false)
+
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/android/example/gobike/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package android.example.gobike.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
